57a09d904a13f97f6370ad4ff3d843c1fc2c6937
Diego Tapia <64498901+dtapia-IT@users.noreply.github.com>
2023-08-29 09:49:45 -0400

    add tests

---
+++ fetch_languages_test.py
@@ -0,0 +1,18 @@
+import unittest
+import requests
+from fetch_languages import fetch_languages  # Make sure to import your function
+
+class TestFetchLanguages(unittest.TestCase):
+    
+    def fetch_languages_test(self):
+        # Replace 'lsst-it/lsst-control' with the target repository
+        language_data = fetch_languages("lsst-it/lsst-control")
+        
+        # Test: Check if the function returns a dictionary
+        self.assertIsInstance(language_data, dict)
+        
+        # Test: Check if the dictionary is not empty
+        self.assertTrue(bool(language_data))
+
+if __name__ == "__main__":
+    unittest.main()
